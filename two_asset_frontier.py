396bb8997fabe18e6e405b52fa56d7d608bce4e7
marcgjg <marc@profmarcgoergen.com>
2025-03-21 11:27:10 +0100

    Update two_asset_frontier.py

--- two_asset_frontier.py
+++ two_asset_frontier.py
@@ -155,7 +155,7 @@ def plot_two_stock_frontier(mu_A, mu_B, sigma_A, sigma_B, corr_AB):
     ax.legend(loc='upper left', bbox_to_anchor=(1.04, 1), borderaxespad=0)
 
     # Possibly increase figure size
-    fig.set_size_inches(7, 4)
+    # fig.set_size_inches(7, 4)
 
     plt.tight_layout()
     st.pyplot(fig)
